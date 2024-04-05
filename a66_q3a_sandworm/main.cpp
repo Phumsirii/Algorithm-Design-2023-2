7a43e168cd90ab1fec5e0f82e0be5a2a40717691
unknown <phumsirisumativit@gmail.com>
2024-04-05 23:27:23 +0700

    Fix Something

--- a66_q3a_sandworm/main.cpp
+++ a66_q3a_sandworm/main.cpp
@@ -1,40 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<vector<bool>>> datas; //[N/S][R][C]
-//datas[N][R][C]=true means we can visit
+vector<vector<bool>> datasN,datasS; //[R][C]
+//datas[R][C]=true means we can visit
 //visited is visited in North pole
 vector<vector<bool>> visited,svisited;
-queue<pair<int,int>> dfs; // row , col
+queue<pair<int,int>> bfs,q; // row , col
 vector<int> dx={-1,1,0,0},dy={0,0,-1,1};
 int r,c,n,ans=0,ndata,wr,wc;
 int main(){
     cin>>r>>c>>n;
     visited.resize(r+1,vector<bool> (c+1,false));
-    datas.resize(2,vector<vector<bool>> (r+1,vector<bool> (c+1,false)));
+    datasN.resize (r+1,vector<bool> (c+1,false));
+    datasS.resize (r+1,vector<bool> (c+1,false));
     svisited.resize(r+1,vector<bool> (c+1,false));
     for(int p=0;p<2;++p){
         for(int i=1;i<=r;++i){
             for(int j=1;j<=c;++j){
                 cin>>ndata;
-                if (ndata==0) datas[p][i][j]=true;
+                if (p==0 && ndata==0) datasN[i][j]=true;
+                else if(ndata==0) datasS[i][j]=true;
             }
         }
     }
-    //iterate to all connected component in North Pole
-    dfs.push(make_pair(1,1));
-    while(!dfs.empty()){
-        int row=dfs.front().first;
-        int col=dfs.front().second;
-        dfs.pop();
+    //iterate through all connected component in North Pole
+    bfs.push(make_pair(1,1));
+    while(!bfs.empty()){
+        int row=bfs.front().first;
+        int col=bfs.front().second;
+        bfs.pop();
         if (!visited[row][col]){
             visited[row][col]=true;
             ++ans;
-            //see all neighbors, push to queue if it's 
+            //see all neighbors, push to queue if it's valid to visit
             for(int i=0;i<4;++i){
                 int targetrow=row+dy[i],targetcol=col+dx[i];
                 //skip if the neighbor can't be visited or out of range or is visited
-                if (targetrow<=0 || targetrow>r || targetcol<=0 || targetcol>c || visited[targetrow][targetcol] || !datas[0][targetrow][targetcol]) continue;
-                dfs.push(make_pair(targetrow,targetcol));
+                if (targetrow<=0 || targetrow>r || targetcol<=0 || targetcol>c || visited[targetrow][targetcol] || !datasN[targetrow][targetcol]) continue;
+                bfs.push(make_pair(targetrow,targetcol));
             }
         }
     } //ans is now no of connected component from (1,1) in the northpole
@@ -43,7 +45,6 @@ int main(){
     for(int i=0;i<n;++i){
         cin>>wr>>wc;
         if (visited[wr][wc] && !svisited[wr][wc]){ //new connected component of the Southpole, can be connected from the North
-            queue<pair<int,int>> q;
             q.push(make_pair(wr,wc));
             int tmp=1; //count connected component of (wr,wc) in the south pole
             svisited[wr][wc]=true;
@@ -53,7 +54,7 @@ int main(){
                 //all neighbors of this node
                 for(int j=0;j<4;++j){
                     int tmptargetr=tmpr+dx[j],tmptargetc=tmpc+dy[j];
-                    if (tmptargetr<=0 || tmptargetr>r || tmptargetc<=0 || tmptargetc>c || svisited[tmptargetr][tmptargetc] || !datas[1][tmptargetr][tmptargetc]) continue;
+                    if (tmptargetr<=0 || tmptargetr>r || tmptargetc<=0 || tmptargetc>c || svisited[tmptargetr][tmptargetc] || !datasS[tmptargetr][tmptargetc]) continue;
                     svisited[tmptargetr][tmptargetc]=true;
                     q.push(make_pair(tmptargetr,tmptargetc));
                     ++tmp;
