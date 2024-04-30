9c4397cb143a833935f0797b34154b57fb3fb315
unknown <phumsirisumativit@gmail.com>
2024-04-30 19:04:50 +0700

    finally finish every grader pbs

--- ex07m2_15puzzle/main.cpp
+++ ex07m2_15puzzle/main.cpp
@@ -1,74 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-//   0
-//3  x  1
-//   2
-vector<int> moveassistancex={0,1,0,-1},moveassistancey={-1,0,1,0};
-vector<int> cantmove={2,3,0,1};
-class bfsdata{
+//  0
+//3 x 1
+//  2
+vector<int> oppomove={2,3,0,1,4},movassistx={0,1,0,-1},movassisty={-1,0,1,0};
+class state{
     public:
-        int heuristic,moved,x,y,prevmove;
-        vector<vector<int>> currentcell;
-        bfsdata(int heuristic,int moved,vector<vector<int>> currentdata,int prevmove,int x,int y){
-            this->heuristic=heuristic;
-            this->moved=moved;
-            this->x=x;
-            this->y=y;
-            this->prevmove=prevmove;
-            this->currentcell=currentdata;
-        }
-};
-class bfsComparator {
-public:
-    bool operator()(const bfsdata& a, const bfsdata& b) const {
-        return a.heuristic>b.heuristic;
-    }
-};
-vector<vector<int>> cell(4,vector<int> (4));
-vector<int> expectedx={3,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2},expectedy={3,0,0,0,0,1,1,1,1,2,2,2,2,3,3,3};
-vector<vector<int>> expected={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
-priority_queue<bfsdata,vector<bfsdata>,bfsComparator> bfs;
-set<vector<vector<int>>> visited;
-int startx,starty;
-int main(){
-    int heucnt=0;
-    for(int i=0;i<4;++i){
-        for(int j=0;j<4;++j){
-            cin>>cell[i][j];
-            if (cell[i][j]==0) startx=j,starty=i;
-            heucnt+=abs(i-expectedy[cell[i][j]]);
-            heucnt+=abs(j-expectedx[cell[i][j]]);
-        }
-    }
-    bfs.push(bfsdata(heucnt,0,cell,-1,startx,starty));
-    while(!bfs.empty()){
-        if (bfs.top().currentcell==expected){
-            cout<<bfs.top().moved;
-            return 0;
-        }
-        vector<vector<int>> currentdata=bfs.top().currentcell;
-        if (visited.find(currentdata)!=visited.end()) continue;
-        int movedcnt=bfs.top().moved;
-        int cx=bfs.top().x;
-        int cy=bfs.top().y;
-        int premove=bfs.top().prevmove;
-        //see all moves
+    vector<vector<int>> board;
+    int stepsdone;
+    int prevmove;
+    int errcnt;
+    int x,y;
+    //constructor
+    state(){};
+    state(int stdo,int prevm,vector<vector<int>> b){
+        stepsdone=stdo;
+        board=b;
+        prevmove=prevm;
+        errcnt=0;
         for(int i=0;i<4;++i){
-            //not moving backward
-            if (i!=-1 && cantmove[premove]==i) continue;
-            int newx=cx+moveassistancex[i],newy=cy+moveassistancey[i];
-            if (newx<0 || newx>3 || newy<0 || newy>3) continue;
-            swap(currentdata[cy][cx],currentdata[newy][newx]);
-            int errcnt=0;
-            for(int k=0;k<4;++k){
-                for(int j=0;j<4;++j){
-                    errcnt+=abs(k-expectedy[currentdata[k][j]]);
-                    errcnt+=abs(j-expectedx[currentdata[k][j]]);
+            for(int j=0;j<4;++j){
+                //[y][x]
+                int tmpdata=b[i][j];
+                if (tmpdata==0){
+                    x=j,y=i;
+                    continue;
                 }
+                errcnt+=(abs(i-(tmpdata-1)/4)+abs(j-(tmpdata-1)%4));
             }
-            bfs.push(bfsdata(errcnt+movedcnt+1,movedcnt+1,currentdata,i,newx,newy));
-            swap(currentdata[cy][cx],currentdata[newy][newx]);
         }
-        bfs.pop();
     }
+    //consider ones with less err first
+    bool operator<(const state &other)const{
+        return errcnt+stepsdone>other.errcnt+other.stepsdone;
+    }
+};
+priority_queue<state> pq;
+int ans=1e9;
+map<vector<vector<int>>,int> done;
+int main(){
+    vector<vector<int>> firstboard(4,vector<int>(4));
+    for(int i=0;i<4;++i){
+        for(int j=0;j<4;++j) cin>>firstboard[i][j];
+    }
+    state firstdata(0,4,firstboard);
+    pq.push(firstdata);
+    while(!pq.empty()){
+        state candistate=pq.top();
+        pq.pop();
+        //if better one is done
+        if (done.find(candistate.board)!=done.end() && done[candistate.board]<=candistate.stepsdone) continue;
+        done[candistate.board]=candistate.stepsdone;
+        //cannot be better than ans
+        if (candistate.errcnt+candistate.stepsdone>=ans) continue;
+        //see all possible moves from this state
+        for(int i=0;i<4;++i){
+            //avoid repeating same state
+            if (i==oppomove[candistate.prevmove]) continue;
+            int newx=candistate.x+movassistx[i],newy=candistate.y+movassisty[i];
+            //invalid move
+            if (newx>=4 || newx<0 || newy>3 || newy<0) continue;
+            vector<vector<int>> newboard=candistate.board;
+            swap(newboard[candistate.y][candistate.x],newboard[newy][newx]);
+            state newstate(candistate.stepsdone+1,i,newboard);
+            //if the new state is perfect
+            if (newstate.errcnt==0){
+                ans=min(ans,newstate.stepsdone);
+            } //if new state can create better ans
+            else if(newstate.errcnt+newstate.stepsdone<ans){
+                pq.push(newstate);
+            }
+            swap(newboard[candistate.y][candistate.x],newboard[newy][newx]);
+        }
+    }
+    cout<<ans;
 }
