ddf789c2c068a3e639db07d6067f7590ca1290eb
unknown <phumsirisumativit@gmail.com>
2024-05-09 22:30:36 +0700

    add:last final pb

--- a66_f1_largest_diff_path/main.cpp
+++ a66_f1_largest_diff_path/main.cpp
@@ -1,1 +1,30 @@
-//will finish it later
+#include <bits/stdc++.h>
+using namespace std;
+int n,m,t1,t2,ans=0,currentstart;
+vector<int> values;
+vector<bool> visited;
+vector<vector<int>> adj;
+void dfs(int no){
+    visited[no]=true;
+    ans=max(ans,values[no]-values[currentstart]);
+    for(int &x:adj[no]){
+        if (visited[x]) continue;
+        dfs(x);
+    }
+}
+int main(){
+    cin>>n>>m;
+    values.resize(n);
+    adj.resize(n);
+    for(int i=0;i<n;++i) cin>>values[i];
+    for(int i=0;i<m;++i){
+        cin>>t1>>t2;
+        adj[t1].push_back(t2);
+    }
+    for(int i=0;i<n;++i){
+        visited=vector<bool> (n,false);
+        currentstart=i;
+        dfs(i);
+    }
+    cout<<ans;
+}
